e75d8deaad339100b8ea941db584673456b852f1
Mimo <mimoriam420@gmail.com>
2024-08-31 21:02:45 +0500

    feat(dashboard): Handle change to onClick of a Pet

--- src/components/page-ui/pet-list.tsx
+++ src/components/page-ui/pet-list.tsx
@@ -2,18 +2,24 @@
 
 import Image from "next/image";
 import { usePetContext } from "@/contexts/pet-context-provider";
+import { cn } from "@/lib/utils";
 
 export default function PetList() {
-  const { pets } = usePetContext();
+  const { pets, selectedPetId, handleChangeSelectedPetId } = usePetContext();
 
   return (
     <ul className="border-light border-b bg-white">
       {pets.map((pet) => (
         <li key={pet.id}>
           <button
-            className={
-              "flex h-[70px] w-full cursor-pointer items-center gap-3 px-5 text-base transition hover:bg-[#EFF1F2] focus:bg-[#EFF1F2]"
-            }
+            onClick={() => handleChangeSelectedPetId(pet.id)}
+            className={cn(
+              "flex h-[70px] w-full cursor-pointer items-center gap-3 px-5 text-base transition hover:bg-[#EFF1F2]" +
+                " focus:bg-[#EFF1F2]",
+              {
+                "bg-[#EFF1F2]": selectedPetId === pet.id,
+              },
+            )}
           >
             <Image
               src={pet.imageUrl}
